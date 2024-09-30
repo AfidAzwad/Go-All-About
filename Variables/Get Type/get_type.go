2d396f0f7446f382d7801c8a4eefc1497b92e3a8
Azwad <mdazwad22@gmail.com>
2024-10-01 00:02:50 +0600

    [ADD] Get Type

---
+++ Variables/Get Type/get_type.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 4.4
+	experience := 2
+
+	fmt.Printf("My age is %T\n", age)             // float64
+	fmt.Printf("My experience is %T", experience) // int
+}
