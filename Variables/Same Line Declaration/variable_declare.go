c47674c9ed4b856847ab96f28b3d59132bad5f11
Azwad <mdazwad22@gmail.com>
2024-10-21 09:56:49 +0600

    [DEL] Empty Main Branch

--- Variables/Same Line Declaration/variable_declare.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	age, experience := 5, 2
-
-	fmt.Printf("My age is %v and experience is %v\n", age, experience)
-}
--- Variables/Type Casting/type_casting.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	account := 2.5
-
-	fmt.Printf("My year of experience is %v !", int(account))
-}
